736b95e65e891978a7e12e0b2dad950ea7bb1edb
Dwlin <dwlin515@gmail.com>
2020-04-22 21:31:47 +0800

    initial

--- 0422 lesson 3/PrimeNum.py
+++ 0422 lesson 3/PrimeNum.py
@@ -1,1 +1,23 @@
 # n 是質數?
+
+n = int(input("數字"))
+check = True
+for i in range(2, n//2+1):
+    print("%d / %d 餘數 %d" % (n, i, n % i))
+    if n % i == 0:
+        check = False
+        break;
+
+print(n, check)
+
+# 2~50 誰是質數
+def isprime(n):
+    check = True
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            check = False
+            break;
+        return check
+
+for i in range(2, 51):
+    print(i, "質數" if isprime(i) else "非質數")
