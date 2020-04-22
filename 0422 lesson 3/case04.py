7874bdc1a0ebdd42edc09b792d1e408fc6b70812
Dwlin <dwlin515@gmail.com>
2020-04-22 19:15:45 +0800

    initial

---
+++ 0422 lesson 3/case04.py
@@ -0,0 +1,11 @@
+def foo():
+    pass
+def bar(n):
+    if n < 60:
+        return #可以終止
+    print("過關")
+
+n = 70
+
+foo()
+bar(n)
---
+++ 0422 lesson 3/case05.py
@@ -0,0 +1,8 @@
+def mask(money) :
+    x = money // 5 #整除
+    size = "成人"
+    return x, size
+
+my_x, my_size = mask(104)
+print(my_x, my_size)
+
